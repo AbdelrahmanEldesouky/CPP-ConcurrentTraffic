4354571510766c8fdb7e468dca9be23f566c5118
AbelrahmanEldesouky <abdelrhman.aldesoky@outlook.com>
2025-02-08 01:39:30 +0200

    solve: FP.5

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -6,24 +6,30 @@
 #include "TrafficLight.h"
 /* Implementation of class "MessageQueue" */
 
-
 template <typename T>
 T MessageQueue<T>::receive()
 {
-    // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
-    // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+  // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait()
+  // to wait for and receive new messages and pull them from the queue using move semantics.
+  // The received object should then be returned by the receive function.
+
+  std::unique_lock<std::mutex> lock(_mutex);
+  _condition.wait(lock, [this]
+                  { return !_queue.empty(); });
+  T msg = std::move(_queue.front());
+  _queue.pop_front();
+  return msg;
 }
 
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
-    // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
-    // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+  // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex>
+  // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
 
-    std::lock_guard<std::mutex> lock(_mutex);
-    _queue.push_back(std::move(msg));
-    _condition.notify_one();
+  std::lock_guard<std::mutex> lock(_mutex);
+  _queue.push_back(std::move(msg));
+  _condition.notify_one();
 }
 
 /* Implementation of class "TrafficLight" */
@@ -32,9 +38,18 @@ TrafficLight::TrafficLight() { _currentPhase = TrafficLightPhase::red; }
 
 void TrafficLight::waitForGreen()
 {
-    // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
-    // runs and repeatedly calls the receive function on the message queue. 
-    // Once it receives TrafficLightPhase::green, the method returns.
+  // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop
+  // runs and repeatedly calls the receive function on the message queue.
+  // Once it receives TrafficLightPhase::green, the method returns.
+
+  while (true)
+  {
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    if (_trafficLightQueue.receive() == TrafficLightPhase::green)
+    {
+      return;
+    }
+  }
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase() { return _currentPhase; }
